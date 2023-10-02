455525380d7d0b1e64f332d1703134e9a6a38543
Your Name <you@example.com>
2023-10-02 10:49:29 +0200

    format

--- frontend/src/routes/Login_form.svelte
+++ frontend/src/routes/Login_form.svelte
@@ -1,12 +1,12 @@
 <script>
-	import { goto } from "$app/navigation";
+  import { goto } from "$app/navigation";
 
-    function buttonTest() {
-        goto ('http://localhost:3000/auth/login');
-    }
+  function buttonTest() {
+    goto("http://localhost:3000/auth/login");
+  }
 </script>
 
 <div class="pixel-border h-96 w-80 bg-white box">
-    <h1 class="text-center font-pixel font-bold text-6xl m-4">LOGIN</h1>
-    <button class="pixel-button" on:click={buttonTest}>Test</button>
+  <h1 class="text-center font-pixel font-bold text-6xl m-4">LOGIN</h1>
+  <button class="pixel-button" on:click={buttonTest}>Test</button>
 </div>
