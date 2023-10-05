4da92378592ce7a35924ab7e4b886e7da4434512
Aingeru Alvarez sanchez <aalvarez@c4r5s4.42urduliz.com>
2023-10-05 20:17:43 +0200

    menu mockup

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,6 @@
     import "../app.css";
 </script>
 
-<div class="bg-darkFHD bg-cover h-[100vh]">
+<div class="bg-lightFHD bg-cover h-[100vh]">
     <slot />
 </div>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div id="page-container" class="flex h-screen">
+    <nav class="flex flex-col justify-center items-center w-[50%] max-w-[558px] min-w-[364px] h-[75%] max-h-[632px] min-h-[442px] m-auto bg-slate-600">
+        <ul class="space-y-[3em]">
+            <li class=""><a href="/">Button 1</a></li>
+            <li class=""><a href="/">Button 2</a></li>
+            <li class=""><a href="/">Button 3</a></li>
+            <li class=""><a href="/">Button 4</a></li>
+        </ul>
+    </nav>
+</div>
