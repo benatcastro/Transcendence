3a1c3ac0534a3203aeb7bc5c96d2a3cb6264839d
Aingeru Alvarez sanchez <aalvarez@c4r5s4.42urduliz.com>
2023-10-05 19:55:17 +0200

    added 4kimage to tailwind backgrounds

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,10 +4,12 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
+        'dark4K': "url('/background/dark/dark4K.jpg')",
         'darkFHD': "url('/background/dark/darkFHD.jpg')",
         'darkHD': "url('/background/dark/darkHD.jpg')",
         'darkTBL': "url('/background/dark/darkTBL.jpg')",
         'darkMD': "url('/background/dark/darkMD.jpg')",
+        'light4K': "url('/background/light/light4K.jpg')",
         'lightFHD': "url('/background/light/lightFHD.jpg')",
         'lightHD': "url('/background/light/lightHD.jpg')",
         'lightTBL': "url('/background/light/lightTBL.jpg')",
