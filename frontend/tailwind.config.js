ff5208dd887042f29e82beeb42b1a3d6963e741b
aalvarez <aingerualvarezsanchez@gmail.com>
2023-10-07 21:22:45 +0200

    new custom definitions added to tailwind.config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./src/**/*.{html,ts,svelte}'],
   theme: {
     extend: {
+      screen: {
+        'desktop': {
+          'raw': '(hover: hover)'
+        }
+      },
+      fontSize: {
+        'primaryl': 'calc(0.3em + 3vh)',
+        'primaryp': 'calc(0.3em + 3vw)',
+        'secodaryl': 'calc(0.3em + 2vh)',
+        'secodaryp': 'calc(0.3em + 2vw)'
+      },
       backgroundImage: {
         'darkFHD': "url('/background/dark/darkFHD.jpg')",
-        'lightFHD': "url('/background/light/lightFHD.jpg')",
+        'lightFHD': "url('/background/light/lightFHD.jpg')"
       },
-    },
+      colors: {
+        'neonred': '#FE0000',
+        'neonyellow': '#FFF205',
+        'neongreen': '#00ff9f',
+        'neonblue': '#00b8ff',
+        'neonpink': '#ea00d9'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
 
