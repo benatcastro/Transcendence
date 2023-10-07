b4afccf9e8f8458e5c645401737d8aba0a6606f9
aalvarez <aingerualvarezsanchez@gmail.com>
2023-10-07 21:31:22 +0200

    added a footer to the general layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
     import "../app.css";
 </script>
 
 <div id="screen-container">
     <slot />
+    <footer class="secondary-button fixed bottom-0 left-1/2 -translate-x-1/2">
+        <a on:click={() => goto("/team")} href="/team">Meet the team</a>
+    </footer>
 </div>
