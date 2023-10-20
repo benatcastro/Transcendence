099d85f0b322bec1367640405a2eaf055170f59e
Anhema <anhema95@gmail.com>
2023-10-21 01:22:17 +0200

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vitest/config'
-import type { UserConfig } from 'vite'
-
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	ssr: {
-		noExternal: ['three', 'troika-three-text'],
-	},
-=======
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -20,5 +6,4 @@ export default defineConfig({
 	ssr: {
 		noExternal: ['three']
 	  }
->>>>>>> main
 });
