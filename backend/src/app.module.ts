66ff70f13964526fb2e29af959442f8916883b4d
Benat Castro <becastro@c3r8s3.42urduliz.com>
2023-10-27 21:47:10 +0200

    throttle implementation - rate limiting

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, ThrottlerModule.forRoot([{
+    ttl: 6000,
+    limit: 10
+    ,
+  }])], // Combinar todas las importaciones en una sola declaración
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
