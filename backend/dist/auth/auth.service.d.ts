49f51a58a553dd904c3c1fdef5faf2d45d8d8b20
Aingeru Alvarez <aingeru.develop@gmail.com>
2023-11-11 16:01:03 +0100

    hirosima

--- backend/dist/auth/auth.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-export declare class AuthService {
-    private readonly httpService;
-    constructor(httpService: HttpService);
-    getLoginRedirectURI(): string;
-    getAccessToken(code: string, state: string): Promise<any>;
-    getUserFromApi(access_token: any): Promise<any>;
-}
--- frontend/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import App from '$lib/components/App.svelte'
-</script>
-  
-  <div class="h-screen w-screen">
-    <App />
-  </div>
-  
