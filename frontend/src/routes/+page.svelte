27916f0d21577da4a3abe8e8d3478a1334f8a1f0
Anhema <anhema95@gmail.com>
2023-11-19 11:45:28 +0100

    basic

--- frontend/src/routes/+page.svelte
+++
@@ -1,4 +0,0 @@
-<script lang="ts">
-</script>
-
-<h1>Page slot</h1>
---
+++ frontend/src/routes/pong/Player1.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import * as Threlte from '@threlte/core'
+    import * as Three from 'three'
+	import { T } from '@threlte/core'
+    import { Collider, RigidBody, AutoColliders } from '@threlte/rapier'
+    import { DEG2RAD } from 'three/src/math/MathUtils.js'
+
+    //let PlayerVelocity = 10;
+    export let PlayerVelocity = 0;
+
+    let playerX = 0;
+
+    let Left = false;
+    let Right = false;
+
+    const onKeyDown = (e: KeyboardEvent) =>
+    {
+        //console.log(e.key);
+        e.preventDefault()
+
+		if (e.key == 'a')
+            Left = true;
+		if (e.key == 'd')
+            Right = true;
+	}
+    const onKeyUp= (e: KeyboardEvent) =>
+    {
+        e.preventDefault()
+        if (e.key == 'a')
+            Left = false;
+		if (e.key == 'd')
+            Right = false;
+	}
+
+    Threlte.useFrame(() =>
+    {
+		if (Left && Right)
+            playerX = 0;
+        else if (Left)
+            playerX = -PlayerVelocity;
+        else if (Right)
+            playerX = PlayerVelocity;
+        else
+            playerX = 0;
+	})
+</script>
+
+<svelte:window on:keydown={onKeyDown} on:keyup={onKeyUp}/>
+
+<!-- PLAYER 1 -->
+<T.Group
+    position={[0, 1, 15]}
+>
+    <RigidBody
+        gravityScale={0}
+        enabledTranslations={[false, false, false]}
+        enabledRotations={[false, false, false]}
+        userData={{tag: 'player1'}}
+        linearVelocity={[playerX, 0, 0]}
+    >
+        <Collider
+        shape={'cuboid'}
+        args={[2, 1, 0.25]}
+        >
+            <T.Mesh let:ref castShadow position={[0, 0, 0.25]}>
+                <T.BoxGeometry args={[5, 2, 1]}/>
+                <T.MeshStandardMaterial color="#ffffff" />
+            </T.Mesh>
+        </Collider>
+        <Collider
+            shape={'cuboid'}
+            args={[0.25, 1, 0.5]}
+        />
+    </RigidBody>
+</T.Group>
