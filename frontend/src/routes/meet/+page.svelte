9fe26d73ce846fb29cdd5a8827dded09adfe2b25
Aingeru <aingeru.develop@gmail.com>
2023-11-27 10:51:37 +0000

    /meet skelleton added

---
+++ frontend/src/routes/meet/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	let images: string[][] = [
+		['Aingeru Álvarez', ''],
+		['Ander Herrero', ''],
+		['Antonio del Corral', ''],
+		['Beñat Castro', ''],
+		['Cristian Bustamante', '']
+	];
+</script>
+
+<svelte:head>
+	<title>Meet the team</title>
+</svelte:head>
+
+<h2>Meet the team behind CyberPong</h2>
+{#each images as image}
+	<section>
+		<img src={image[1]} alt={image[0]} />
+		<p>Description for {image[0]}</p>
+	</section>
+{/each}
+
+<style>
+	/*
+        The names are organized alphabetically, which I think makes sense if
+        it supposed to simulate a futuristic terminal screen.
+    */
+
+	/*
+        I will create a consistent background simulating the screen of some
+        futuristic-like terminal, with a preview of everyone in the team, when
+        hovering over one of the sections, it will display the whole paragraph
+        with the general contribution of each one.
+    */
+
+	/* P.S.: Do not fucking touch my styles or I will eat your god damn eyeballs */
+</style>
