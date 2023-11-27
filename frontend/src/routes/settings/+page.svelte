3ef1feac0a23ac5a72d882cb25febcc22ddcdb1f
Aingeru <aingeru.develop@gmail.com>
2023-11-27 10:52:36 +0000

    /setting skelleton added

---
+++ frontend/src/routes/settings/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	let menuOpts: string[] = ['Volume', 'Graphics', 'Game'];
+</script>
+
+<svelte:head>
+	<title>Settings</title>
+</svelte:head>
+
+<h2>Settings</h2>
+<menu>
+	<!-- Volume on click opens sub-menu for music and sound effects -->
+	<!-- Graphics click opens sub-menu for animations and effects, as well as general graphics -->
+	<!-- Game click opens sub-menu for skins and personalization -->
+	{#each menuOpts as opt}
+		<li>{opt}</li>
+	{/each}
+</menu>
+<a on:click={() => goto('/')} href="/">&lt;</a>
+
+<style>
+	/*
+        Not decided yet, probably will try to simulate some electronic components
+        and reflect how the electronics change as the settings do.
+
+        I.E. You turn up the volume, and a bar connected to a sound system in the
+        background goes up and changes on a gradient. Similar to electronic music
+        components.
+    */
+
+	/* P.S.: Do not fucking touch my styles or I will eat your god damn eyeballs */
+</style>
