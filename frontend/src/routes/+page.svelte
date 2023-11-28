04e4a40ca98de248ebcb3b58424fdf99f3bbc1a4
Aingeru <aingeru.develop@gmail.com>
2023-11-28 14:15:02 +0000

    mockup for login added, improved skelleton

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,39 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-    let openPlayMenu: boolean = false;
-    let isLoggedIn: boolean;
+	let openPlayMenu: boolean = false;
+	let isLoggedIn: boolean; //TODO cookie management?
+	let menuItems = [
+		{ name: 'Play', link: '/', action: () => (openPlayMenu = true) },
+		{ name: 'Log in', link: '/', action: () => handleLoginClick() },
+		{ name: 'Log out', link: '/', action: () => (isLoggedIn = false) },
+		{ name: 'Profile', link: '/profile' },
+		{ name: 'Leaderboard', link: '/leaderboard' },
+		{ name: 'Settings', link: '/settings' }
+	];
 
+	$: menuOpts = isLoggedIn
+		? menuItems.filter((item) => item.name !== 'Log in')
+		: menuItems.filter((item) => item.name !== 'Log out');
+
+	async function handleLoginClick() {
+		console.log('Logging in...');
+		return new Promise<void>((resolve, reject) => {
+			//TODO fetch user info with promise
+			//User authentication succeeded
+			isLoggedIn = true;
+            resolve();
+			reject(new Error('Login failed'));
+			//User authentication failed
+		});
+	}
 	function handlePlayClick(option: string) {
-		goto(`/matchmaking?mode=${option}`);
+		if (option === 'ranked' && !isLoggedIn) {
+			handleLoginClick()
+				.then(() => goto(`/matchmaking?mode=${option}`))
+				.catch((e) => console.error(e.message));
+		} else {
+			goto(`/matchmaking?mode=${option}`);
+		}
 	}
 </script>
 
@@ -15,19 +44,20 @@
 <h1>Main menu</h1>
 <nav>
 	<ul>
-		<li><a on:click={() => openPlayMenu = true } href="/">Play</a></li>
-		<li><a href="/">Log in</a></li>
-		<li><a href="/">Log out</a></li>
-		<li><a href="/profile">Profile</a></li>
-		<li><a href="/leaderboard">Leaderboard</a></li>
-		<li><a href="/settings">Settings</a></li>
+		{#each menuOpts as option}
+			{#if option.link === '/'}
+				<li><a on:click|preventDefault={option.action} href={option.link}>{option.name}</a></li>
+			{:else}
+				<li><a href={option.link}>{option.name}</a></li>
+			{/if}
+		{/each}
 	</ul>
 </nav>
 
 {#if openPlayMenu}
-    <div class="play-menu">
-        <button on:click={() => handlePlayClick("casual")}>Casual</button>
-        <button on:click={() => handlePlayClick("ranked")}>Ranked</button>
-        <button on:click={() => openPlayMenu = false}>Go back</button>
-    </div>
+	<div>
+		<button on:click={() => handlePlayClick('casual')}>Casual</button>
+		<button on:click={() => handlePlayClick('ranked')}>Ranked</button>
+		<button on:click={() => (openPlayMenu = false)}>Go back</button>
+	</div>
 {/if}
