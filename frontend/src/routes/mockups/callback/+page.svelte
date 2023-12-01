499ad9d7c9f2631bf097157f6ff6cbd094bb28a8
Benat Castro <benatcastro@gmail.com>
2023-12-01 20:13:11 +0100

    post to backend

--- frontend/src/routes/mockups/callback/+page.svelte
+++ frontend/src/routes/mockups/callback/+page.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
-    import type {PageData} from './$types';
+    import { onMount } from "svelte";
+    import { page } from '$app/stores';
 
-    export let data: PageData;
+    onMount(async () => {
+        const code  = $page.url.searchParams.get('code');
+        const endpoint = "http://localhost:80/auth/42";
+        const queryParams = new URLSearchParams();
+
+        if (code)
+            queryParams.set('code', code);
+
+        const response = await fetch(endpoint+ "?" + queryParams, {method: "POST"});
+        const data = await response.json();
+
+        console.log("callback post data: ", data);
+    })
+
 </script>
 
 <h1>hey this is a callback</h1>
