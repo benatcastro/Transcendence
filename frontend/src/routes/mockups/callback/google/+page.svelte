0922f694f79c4c7229284b7342d9c0ba42ff8b8c
Benat Castro <benatcastro@gmail.com>
2023-12-05 19:29:14 +0100

    mockups for auth callback

---
+++ frontend/src/routes/mockups/callback/google/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import type {PageData} from './$types';
+    import { page } from '$app/stores';
+
+    const code = $page.url.searchParams.get('code');
+     async function getToken() {
+         try {
+             const response = await fetch('http://localhost:8000/auth/google/', {
+                 method: 'POST',
+                 headers: {
+                     'Content-Type': 'application/json',
+                 },
+                 body: JSON.stringify({ "code": code }),
+             });
+
+             if (response.ok) {
+                 data = await response.json();
+                 console.log('Code posted successfully');
+                 console.log(data);
+             } else {
+                 console.error('Failed to post code');
+             }
+         } catch (error) {
+             console.error('Error posting code:', error);
+         }
+     }
+    getToken()
+    export let data: PageData;
+</script>
+
+<h1>Google callback</h1>
+<h3>code: {code}</h3>
