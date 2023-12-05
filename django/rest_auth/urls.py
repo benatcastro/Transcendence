2505fe81eb8790fdb416e019e154547904b5e788
Benat Castro <benatcastro@gmail.com>
2023-12-05 17:29:36 +0100

    User creation fix in 42intra provider

--- django/rest_auth/urls.py
+++ django/rest_auth/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from .views import GoogleLogin, FtIntraLogin
 
 urlpatterns = [
-
     path('', include('dj_rest_auth.urls')),
     path('', include('allauth.urls')),
+    path('registration/', include('dj_rest_auth.registration.urls')),
     path('google/', GoogleLogin.as_view(), name='google_login'),
     path('42intra/', FtIntraLogin.as_view(), name='42intra_login')
 ]
