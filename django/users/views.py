54f1a752c945781ae602e53883bcb5b82055c555
Benat Castro <benatcastro@gmail.com>
2023-12-07 13:37:10 +0100

    lookup field updated

--- django/users/views.py
+++ django/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.mixins import ( CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin )
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from rest_framework.filters import SearchFilter
+from rest_framework.exceptions import MethodNotAllowed
 
-# Create your views here.
+from .models import TranscendenceUser
+from .serializers import TranscendenceUserSerializer
+
+
+class TranscendenceUserViewSet(ModelViewSet):
+    filter_backends = [SearchFilter]
+    serializer_class = TranscendenceUserSerializer
+    queryset = TranscendenceUser.objects.all()
+    search_fields = ["username"]
+    lookup_field = 'username'
+
+    def create(self, request, *args, **kwargs):
+        raise MethodNotAllowed("POST")
+
