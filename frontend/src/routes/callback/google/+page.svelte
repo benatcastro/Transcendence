846f94a976317eb51f0aab7a8f5f5e521d06e01f
Benat Castro <benatcastro@gmail.com>
2023-12-19 21:53:39 +0100

    google callback page updated

--- frontend/src/routes/callback/google/+page.svelte
+++ frontend/src/routes/callback/google/+page.svelte
@@ -1,29 +1,35 @@
 <script lang="ts">
     import type {PageData} from './$types';
     import { page } from '$app/stores';
+    import { goto } from "$app/navigation";
 
     const code = $page.url.searchParams.get('code');
-     async function getToken() {
-         try {
-             const response = await fetch('http://localhost:8000/auth/google/', {
-                 method: 'POST',
-                 headers: {
-                     'Content-Type': 'application/json',
-                 },
-                 body: JSON.stringify({ "code": code }),
-             });
+    async function getToken() {
+        try {
+            const response = await fetch('http://localhost:8000/auth/google/', {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ "code": code }),
+            });
 
-             if (response.ok) {
-                 data = await response.json();
-                 console.log('Code posted successfully');
-                 console.log(data);
-             } else {
-                 console.error('Failed to post code');
-             }
-         } catch (error) {
-             console.error('Error posting code:', error);
-         }
-     }
+            if (response.ok) {
+                data = await response.json();
+                console.log('Code posted successfully');
+                console.log(response.headers)
+                console.log(data);
+                goto('/')
+
+            } else {
+                console.log(data)
+                console.error('Failed to post code');
+            }
+        } catch (error) {
+            console.error('Error posting code:', error);
+        }
+    }
     getToken()
     export let data: PageData;
 </script>
