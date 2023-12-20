217dd5c390af6fd25d7b8958aaa84278aff0b7f1
Aingeru <aingeru.develop@gmail.com>
2023-12-20 22:30:36 +0100

    Added music player and background image to general layout.
    Signed-off-by: Aingeru Alvarez <aingeru.develop@gmail.com>

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,38 @@
+<script lang="ts">
+	const AUDIO_SRC = '/sound/Half_Mystery.mp3';
+	const IMAGE_SRC = '/assets/green3.png';
+	let musicPlayer;
+	const playMusic = () => {
+		if (musicPlayer) {
+			musicPlayer.play();
+		}
+	};
+</script>
+
+<audio
+	bind:this={musicPlayer}
+	on:ended={() => musicPlayer && musicPlayer.play()}
+	src={AUDIO_SRC}
+	preload="auto"
+/>
+
+<div class="fixed-top text-left">
+	<button class="btn btn-outline- shadow-none p-0" on:click={playMusic}>
+		<img loading="lazy" src={IMAGE_SRC} alt="Toggle music on/off button" class="w-header" />
+	</button>
+</div>
+
 <slot />
 
 <footer class="fixed-bottom text-center py-3">
 	<a href="/meet">Meet the team</a>
 </footer>
+
+<style>
+	:global(body) {
+		background-image: url('/assets/lightFHD.jpg');
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+</style>
