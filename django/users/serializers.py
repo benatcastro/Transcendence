6403d8a9b8e76189b34ca3958bf02022f38cae98
Aingeru Alvarez sanchez <aalvarez@c3r7s2.42urduliz.com>
2024-01-09 21:13:23 +0100

    nuevo main

--- django/users/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import TranscendenceUser
-from rest_framework.serializers import ModelSerializer
-
-
-class TranscendenceUserSerializer(ModelSerializer):
-    class Meta:
-        model = TranscendenceUser
-        fields = ("id", "username", "email")
