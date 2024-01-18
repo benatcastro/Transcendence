ff2ad28d94963e8376e0f7750e7bea4f710a0f56
Aingeru Alvarez <aingeru.develop@gmail.com>
2024-01-18 21:19:48 +0100

    Deleted unused components, enhanced matchmaking flow, refactored callbacks. Better main menu.
    Signed-off-by: Aingeru Alvarez <aingeru.develop@gmail.com>

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,29 +1,3 @@
-<script lang="ts">
-	import { goto } from '$app/navigation';
-	import { browser } from '$app/environment';
-	import { loginStorage } from '$lib/stores/stores';
-	import PlayModal from '$lib/components/PlayModal.svelte';
-	import Button from '$lib/components/Button.svelte';
-
-	let menuItems = [
-		{ component: PlayModal, props: { gradient: 'aqua' } },
-		{
-			component: Button,
-			props: { title: 'Profile', gradient: 'aqua', href: '/profile' }
-		},
-		{
-			component: Button,
-			props: { title: 'Leaderboard', gradient: 'aqua', href: '/rank' }
-		}
-	];
-
-	loginStorage.subscribe((loginSelection) => {
-		if (browser && loginSelection) {
-			goto(`http://localhost:8000/auth/${loginSelection}/login`);
-		}
-	});
-</script>
-
 <svelte:head>
 	<title>CyberPong</title>
 	<meta
@@ -40,38 +14,116 @@
 	/>
 </svelte:head>
 
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import { browser } from '$app/environment';
+    import { loginStorage } from '$lib/stores/stores';
+
+    let showModal: boolean = false;
+    let isLoggedIn: boolean = false;
+    let username: string = '';
+
+    loginStorage.subscribe((loginSelection) => {
+        if (browser && loginSelection) {
+            goto(`http://localhost:8000/auth/${loginSelection}/login`);
+        }
+    });
+
+    function toggleModal() {
+        showModal = !showModal;
+    }
+
+	async function getUser() {
+		if (!isLoggedIn) {
+			const res = await fetch('http://localhost:8000/matchmaking/create-usr?mode=casual');
+			if (!res.ok) {
+				throw new Error('Error while creating user');
+			}
+			username = (await res.json()).user;
+		}
+		return username;
+	}
+
+	async function handlePlayClick(option: string) {
+		try {
+			if (option !== 'ranked' || isLoggedIn) {
+				await getUser();
+				await goto(`/matchmaking?mode=${option}&user=${username}`);
+			}
+		} catch (e) {
+			console.error(e.message);
+		}
+	}
+</script>
+
 <div class="d-flex flex-column flex-center">
-	<nav>
-		<ul class="list-unstyled d-flex flex-column align-items-center">
-			<li class="font-xe btn-lg w-100">
-				<div class="card mb-3 p-5 m-3 btn cyberpunk-background">
-					<svelte:component this={menuItems[0].component} {...menuItems[0].props} />
-				</div>
-			</li>
-			<li class="d-flex justify-content-around w-100">
-				<div class="card mb-3 p-5 m-3 btn cyberpunk-background">
-					<svelte:component this={menuItems[1].component} {...menuItems[1].props} />
-				</div>
-				<div class="card mb-3 p-5 m-3 btn cyberpunk-background">
-					<svelte:component this={menuItems[2].component} {...menuItems[2].props} />
-				</div>
-			</li>
-		</ul>
-	</nav>
+    <nav>
+        <ul class="list-unstyled d-flex flex-column align-items-center">
+            <li class="font-xe btn-lg w-100">
+                <div class="card mb-3 p-5 m-3 btn cyberpunk-background" role="button" on:click={toggleModal}>
+                    <button class="btn btn-link border-0"><span>Play</span></button>
+                </div>
+                {#if showModal}
+                    <div class="modal-background" on:click={toggleModal}>
+                        <div class="modal-content" on:click|stopPropagation>
+                            <p class="modal-title">Select Mode</p>
+                            <button type="button" class="btn btn-primary" on:click={() => handlePlayClick('casual')}>Casual</button>
+                            <button type="button" class="btn btn-secondary" on:click={() => handlePlayClick('ranked')}>Ranked</button>
+                        </div>
+                    </div>
+                {/if}I
+            </li>
+            <li class="font-xe d-flex justify-content-around w-100">
+                <div class="card mb-3 p-5 m-3 btn cyberpunk-background" role="button" on:click={() => goto('/profile')}>
+                    <button class="btn btn-link border-0"><span>Profile</span></button>
+                </div>
+                <div class="card mb-3 p-5 m-3 btn cyberpunk-background" role="button" on:click={() => goto('/rank')}>
+                    <button class="btn btn-link border-0"><span>Leaderboard</span></button>
+                </div>
+            </li>
+        </ul>
+    </nav>
 </div>
 
 <style>
+    .modal-background {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 1;
+    }
+
+    .modal-content {
+        background: white;
+        padding: 1em;
+        max-width: 300px;
+        margin: 0 auto;
+        z-index: 2;
+    }
+
 	.cyberpunk-background {
 		background: linear-gradient(90deg, rgba(204,0,255,0.8) 35%, rgba(0,255,255,0.8) 100%);
 		border: 1px solid #00ffff;
 		box-shadow: 0 0 10px #00ffff, 0 0 20px #00ffff, 0 0 30px #00ffff, 0 0 40px #00ffff;
 	}
 
-	.btn:hover {
-		background-color: rgba(0, 255, 255, 0.5);
-		border-color: #00ffff;
-		box-shadow: 0 0 10px #00ffff, 0 0 20px #00ffff, 0 0 30px #00ffff, 0 0 40px #00ffff;
-	}
+    div.btn:hover {
+        background-color: rgba(0, 255, 255, 0.5);
+        border-color: #00ffff;
+        box-shadow: 0 0 10px #00ffff, 0 0 20px #00ffff, 0 0 30px #00ffff, 0 0 40px #00ffff;
+    }
+
+    div.btn button:hover {
+        background-color: transparent;
+        border-color: transparent;
+        box-shadow: none;
+    }
 
 	.btn {
 		background-color: transparent;
--- frontend/src/routes/callback/42intra/+page.svelte
+++ frontend/src/routes/callback/42intra/+page.svelte
@@ -4,41 +4,34 @@
 	import { onMount } from 'svelte';
     import { goto } from "$app/navigation";
 
+    export let data: PageData;
+
     const code = $page.url.searchParams.get('code');
     let body;
 
-    onMount( async() => {
-        async function getToken() {
-            try {
-                const response = await fetch('http://localhost:8000/auth/42intra/', {
-                    method: 'POST',
-                    credentials: 'include',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ "code": code }),
-                });
+    async function getToken() {
+        try {
+            const response = await fetch('http://localhost:8000/auth/42intra/', {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ "code": code }),
+            });
+            if (response.ok) {
+                data = await response.json();
+                await goto('/')
 
-                if (response.ok) {
-                    data = await response.json();
-                    console.log('Code posted successfully');
-                    console.log(response.headers)
-                    console.log(data);
-                    goto('/')
-
-                } else {
-                    console.log(data)
-                    console.error('Failed to post code');
-                }
-            } catch (error) {
-                console.error('Error posting code:', error);
+            } else {
+                console.error('Failed to post code');
             }
+        } catch (error) {
+            console.error('Error posting code:', error);
         }
-        getToken()
-    })
-    export let data: PageData;
-</script>
+    }
 
-<h1>42intra callback</h1>
-<h3>code: {code}</h3>
-<p>{body}</p>
+    onMount(() => {
+        getToken();
+    })
+</script>
