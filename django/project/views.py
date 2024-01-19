0a9efcf4ab0318dd3ef7b8eb749995fe5ed21b89
Andres Cadena <cbust4ma@student.42urduliz.com>
2024-01-19 18:08:15 +0100

    token

--- django/project/views.py
+++ django/project/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-rom rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 class ObtainAuthToken(APIView):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
