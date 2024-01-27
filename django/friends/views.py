bf4fc186c6ae9be677d5b9663f0d85502b9bd63d
becastro <benatcastro@gmail.com>
2024-01-27 22:17:15 +0100

    Friend Functionality: Implementation of a friend serializer and endpoints for retrieving creating and deleting friends from a user

--- django/friends/views.py
+++ django/friends/views.py
@@ -1,33 +1,65 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
 from users.models import TranscendenceUser
-from users.serializers import TranscendenceUserSerializer
-
+from .serializers import TranscendenceUserFriendSerializer
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+from rest_framework import status
+from rest_framework.mixins import RetrieveModelMixin
 
 # Create your views here.
 
-class TranscendenceUserFriendViewSet(ModelViewSet):
+class TranscendenceUserFriendViewSet(GenericViewSet, RetrieveModelMixin):
     """
     Endpoints  for Retrieving, creating and deleting friend for a user
 
     These endpoints requiere standard authentication
     """
     # permission_classes = [IsAuthenticated]
-    serializer_class = TranscendenceUserSerializer
+    lookup_field = 'username'
+    serializer_class = TranscendenceUserFriendSerializer
     queryset = TranscendenceUser.objects.all()
 
     def create(self, request, *args, **kwargs):
         """
+        param1 --  from_user: the user that will be adding the friend
+        param2 -- to_user: the user that will be added
 
         "to_user" will be  added to the "from_user" friend list and viceversa.
-
-        Params:
-            - from_user: the user that will be adding the friend
-            - to_user: the user that will be added
+        ---
+        parameters:
+            - name: name
+              description: Foobar long description goes here
+              required: true
+              type: string
+              paramType: form
         """
 
-        to_user = self.request.data.get('to_user')
-        from_user = self.request.data.get('from_user')
-        print(from_user, "->", to_user)
+        to_user = get_object_or_404(TranscendenceUser, username=self.request.data.get('to_user'))
+        from_user = get_object_or_404(TranscendenceUser, username=self.request.data.get('from_user'))
 
-        return TranscendenceUser.objects.all()
+        if to_user == from_user:
+            return Response(data={'message': "Error: from user and to_user are the same"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        to_user.friends.add(from_user)
+
+        return Response(data={'message': f'{from_user} and {to_user} friendship added succesfully'}, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+            "to_user" will be deleted from the "from_user" friend list and viceversa.
+        """
+
+        to_user = get_object_or_404(TranscendenceUser, username=self.request.data.get('to_user'))
+        from_user = get_object_or_404(TranscendenceUser, username=self.request.data.get('from_user'))
+
+        if to_user == from_user:
+            return Response(data={'message': "Error: from user and to_user are the same"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        from_user.friends.remove(to_user)
+
+        return Response(data={'message': f'{from_user} and {to_user} friendship deleted successfully'},
+                        status=status.HTTP_200_OK)
