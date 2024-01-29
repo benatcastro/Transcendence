6b3f9018420e74db55ef3b7923657d6cd0e10638
Benat Castro <benatcastro@gmail.com>
2024-01-29 21:20:54 +0100

    User Status Functionality: added last_activity field to user model. Every time the user makes a request this field updates to the current time. The serializer checks if the users last activity was made longer than the USER_AWAY_THERSHOLD_MINUTES constant in settings.py

--- django/users/serializers.py
+++ django/users/serializers.py
@@ -1,8 +1,26 @@
 from .models import TranscendenceUser
 from rest_framework.serializers import ModelSerializer
+from django.utils import timezone
+from django.conf import settings
+
+
+def is_user_online(user):
+    threshold = timezone.now() - timezone.timedelta(minutes=settings.USER_AWAY_THRESHOLD_MINUTES)
+    try:
+        return user.last_activity > threshold
+    except TranscendenceUser.DoesNotExist:
+        return False
 
 
 class TranscendenceUserSerializer(ModelSerializer):
     class Meta:
         model = TranscendenceUser
         fields = ("id", "username", "email", "profile_picture", "score")
+
+    def to_representation(self, instance):
+        user_representation = super().to_representation(instance)
+
+        # Add the status field
+        user_representation['status'] = "online" if is_user_online(instance) else "away"
+
+        return user_representation
