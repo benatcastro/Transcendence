d5d5a980852da29b2ab136faae60335c06056034
Benat Castro <benatcastro@gmail.com>
2024-02-03 14:21:47 +0100

    Functionality: Added friend search bar

--- frontend/src/routes/me/+page.svelte
+++ frontend/src/routes/me/+page.svelte
@@ -21,6 +21,8 @@
 
 <script lang="ts">
     import type {PageData} from './$types';
+    import {addFriends} from "$lib/utilities/utilities";
+    import {modifyUser} from "$lib/utilities/utilities";
 
     export let data: PageData;
     const user = data.user;
@@ -31,32 +33,57 @@
 
     let selectedFile;
     let fileInput;
-    let newEmail: string = '';
-    let newPassword: string = '';
-    let newUsername: string = '';
+    let newUsername: string = user.username;
+
+    let searchInput = "";
 
     async function uploadImage() {
         const formData = new FormData();
         formData.append('image', selectedFile[0]);
 
-        const response = await fetch('http://localhost:8000/users/uploadImage', {
-            method: 'POST',
-            body: formData
+        console.log(formData)
+        const response = await fetch(`http://localhost:8000/users/${user.username}/`, {
+            method: 'PATCH',
+            credentials: 'include',
+            body: formData,
+            // body: JSON.stringify({ image: formData})
         });
 
         if (response.ok) {
             const updatedUser = await response.json();
             user.image = updatedUser.image;
         }
+        console.log(response)
     }
 
     function triggerFileInput() {
         fileInput.click();
     }
 
+    let searchingUsers = "";
+    async function updateSearchedUserList() {
+        if (searchInput === undefined)
+            return
+
+        let url = new URL("http://localhost:8000/users/?")
+        let params = new URLSearchParams(url)
+        params.append("search", searchInput)
+
+        console.log(url + params)
+        const response = await fetch(url + params, {
+            method: 'GET',
+            credentials: 'include',
+        });
+
+        if (response.ok) {
+            searchingUsers = await response.json();
+        }
+        // console.log(response)
+    }
 
 
     async function changeUsername() {
+        console.log("new username: ", newUsername)
         const response = await fetch(`http://localhost:8000/users/${user.username}/`, {
             method: 'PATCH',
             credentials: 'include',
@@ -71,23 +98,25 @@
             console.log(updatedUser)
             $: {
                 user.username = updatedUser.username;
-                newUsername = '';
-                editing = !editing
             }
         }
+        editing = false
+        console.log(response)
     }
 </script>
 {#if user !== "404"}
 <div class="wrapper">
     <div class="d-flex center h-70 w-70 cyberpunk-container">
         <div class="flex-column mt-5 mx-5">
+
+            <h1 class="cyber-text center">User Details</h1>
             <div class="pfp-wrapper center">
-                <img class="profile-image m-5" src={user.image || '/assets/blank_profile.jpg'} alt="Profile image" on:click={triggerFileInput}>
+                <img class="profile-image m-5" src={user.pfp} alt="Profile image" on:click={triggerFileInput}>
                 <span class="change-image-text">Change image</span>
             </div>
             <input type="file" bind:files={selectedFile} bind:this={fileInput} style="display: none;" on:change={uploadImage} />
             <div class="edit-username-wrapper">
-                <input class="placeholder p-2 m-2" value={user.username} readonly={!editing}/>
+                <input bind:value={newUsername} class="placeholder p-2 m-2" placeholder={user.username} readonly={!editing}/>
                 <button on:click={editing ? changeUsername : () => editing = !editing} class="btn btn-primary">
                     {#if editing}
                         <span class="material-symbols-outlined">check</span>
@@ -96,26 +125,88 @@
                     {/if}
                 </button>
             </div>
+            <p class="placeholder p-2 m-2">{user.email}</p>
             <p class="placeholder p-2 m-2">{user.score}</p>
         </div>
+    </div>
+
+    <div class="d-flex center h-70 w-70 cyberpunk-container">
+        <div class="user-search">
+            <div class="flex-column mt-5 mx-5">
+                <h1 class="cyber-text">Search Users</h1>
+                <input type="text"  placeholder="Search Friends" bind:value={searchInput} on:input={updateSearchedUserList} class="placeholder p-2 m-2">
+                {#each searchingUsers as search_user}
+                    {#if user.username !== search_user.username}
+                    <div>
+                        <div class="user-search">
+                            <img class="user-search-pfp" src={search_user.pfp} alt="pfp">
+                            <a href={"http://localhost:5173/profile/" + search_user.username}><h4>{search_user.username}</h4></a>
+                        </div>
+                        <div class="user-search">
+                            <h5>{search_user.status}</h5>
+                            {#if !(user.friends.includes(search_user.id))}
+                                <button type="button" class="btn btn-primary btn-sm" on:click={() => addFriends(user.username, search_user.username)}>Add Friend</button>
+                            {:else}
+                                <p>Already friends</p>
+                            {/if}
+
+                        </div>
+
+                    </div>
+                        {/if}
+                {/each}
+
+            </div>
+
+        </div>
+    </div>
+    <div class="d-flex center h-70 w-70 cyberpunk-container">
+        <div class="center mt-5 mx-5">
+           <h1 class="cyber-text">Match History</h1>
+       </div>
     </div>
 </div>
 {:else}
     <div class="center h-100">
-        <h1>User 404</h1>
+        <h1 class="cyber-text">User 404</h1>
     </div>
 {/if}
 
 <style>
 
+    .user-search {
+        display: flex;
+        gap: 1em;
+        align-items: center;
 
+    }
+
+    .user-search-pfp {
+        border-radius: 50%;
+        width: 3em;
+        height: 3em;
+        object-fit: cover;
+
+    }
+
+    .cyber-text {
+        color: #0ff;
+        font-style: italic;
+        padding: 10px;
+        text-shadow: 0 0 10px #0ff, 0 0 20px #0ff, 0 0 30px #0ff, 0 0 40px #0ff;
+
+    }
 
     .wrapper {
+        margin-top: 15em;
         height: 100vh;
+        width: 100vw;
         display: flex;
         justify-content: center;
         justify-items: center;
         align-items: center;
+        flex-direction: column;
+        gap: 5em;
     }
 
     .center {
@@ -129,6 +220,7 @@
         display: grid;
         grid-template-columns: auto 8em;
     }
+
     .placeholder {
         color: #0ff;
         font-style: italic;
@@ -158,5 +250,6 @@
         background: linear-gradient(90deg, rgba(255,0,255,0.7) 0%, rgba(0,255,255,0.7) 50%, rgba(255,0,255,0.7) 100%);
         border: 7px solid;
         border-image: linear-gradient(90deg, rgb(146, 194, 208) 0%, rgb(69, 141, 210) 50%, rgb(137, 166, 175) 100%) 1;
+        min-width: 30%;
     }
 </style>
