9974d432f9cc359a7c14fdd922058a842da4d7fa
Benat Castro <benatcastro@gmail.com>
2024-02-03 14:22:04 +0100

    Functionality: Added friend fetching

--- frontend/src/routes/me/+page.ts
+++ frontend/src/routes/me/+page.ts
@@ -1,21 +1,10 @@
+
+import {get_user_and_refresh} from "$lib/utilities/utilities";
 export async function load(){
 
+
     const data = async () => {
-        try {
-            const response = await fetch("http://localhost:8000/users/me", {
-                credentials: 'include',
-            });
-            if (response.ok) {
-                const data = await response.json();
-                return {user: data, status: 'ok'};
-            }
-            else {
-                return {user: '404', status: response.status};
-            }
-        }
-        catch (e) {
-            return {user: '404', status: 'fetch error'};
-        }
+        return await get_user_and_refresh();
     }
     return await data()
 }
