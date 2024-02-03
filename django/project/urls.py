b84349cfb0d8e6a4ed1d10355c146193ba27300c
Benat Castro <benatcastro@gmail.com>
2024-02-03 01:06:20 +0100

    Images: added the url needed to access images

--- django/project/urls.py
+++ django/project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -30,4 +32,4 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('', include('django_prometheus.urls')),
     path('transcendence/', include('transcendence.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
